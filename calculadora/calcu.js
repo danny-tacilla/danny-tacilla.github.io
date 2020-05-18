2e7e44f33f3a41b53ffc6ba33cb4b3e0ac7d1501
DannyTCh7 <50375642+DannyTCh7@users.noreply.github.com>
2020-05-18 16:37:50 -0500

    Add files via upload

---
+++ calculadora/calcu.js
@@ -0,0 +1,39 @@
+let setResultado = (valor) => {
+  document.getElementById("resultado").innerHTML = valor;
+};
+let getResultado = () => {
+  return document.getElementById("resultado").innerHTML;
+};
+let agregar = (data) => {
+  let r = getResultado();
+  if (r != "0" || isNaN(data)) {
+    setResultado(r + data);
+  } else {
+    setResultado(data);
+  }
+};
+let raiz = (d) => {
+  let r = getResultado();
+  if (r != 0 || isNaN(d)) {
+    setResultado(Math.sqrt(r).toFixed(5));
+  } else {
+    setResultado(d);
+  }
+};
+let borrar = (b) => {
+  let va = getResultado();
+  let borrar = va.substring(0, va.length - 1);
+  if (borrar == 0) {
+    setResultado(0);
+  } else {
+    setResultado(borrar);
+  }
+};
+let calcular = () => {
+  let r = eval(getResultado());
+
+  setResultado(r);
+};
+let limpiar = () => {
+  setResultado(0);
+};
